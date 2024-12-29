309b162f8518420f1ecd20a6461915d15dac2091
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-29 16:31:12 -0600

    Update navigation_mode.h

--- components/shys_m5_dial/navigation_mode.h
+++ components/shys_m5_dial/navigation_mode.h
@@ -12,6 +12,32 @@ class NavigationMode {
 public:
     NavigationMode(int max_components) : max_components_(max_components) {}
 
+    void handle_rotary_knob(const char *direction, int &currentDevice) {
+        if (is_navigation_mode_) {
+            if (strcmp(direction, ROTARY_LEFT) == 0) {
+                currentDevice--;
+                if (currentDevice < 0) {
+                    currentDevice = max_components_ - 1; // Wrap around
+                }
+            } else if (strcmp(direction, ROTARY_RIGHT) == 0) {
+                currentDevice++;
+                if (currentDevice >= max_components_) {
+                    currentDevice = 0; // Wrap around
+                }
+            }
+        }
+    }
+
+    void handle_button_press(const char *press_type) {
+        if (strcmp(press_type, BUTTON_LONG) == 0) {
+            if (is_navigation_mode_) {
+                exit_navigation_mode();
+            } else {
+                enter_navigation_mode();
+            }
+        }
+    }
+
     void enter_navigation_mode() {
         is_navigation_mode_ = true;
         ESP_LOGI("NAVIGATION_MODE", "Entered navigation mode");
