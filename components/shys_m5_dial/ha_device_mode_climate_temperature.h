9e00fde5b9538b7c009e2008c8d9afb576987b4a
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-29 14:49:43 -0600

    Update ha_device_mode_climate_temperature.h

--- components/shys_m5_dial/ha_device_mode_climate_temperature.h
+++ components/shys_m5_dial/ha_device_mode_climate_temperature.h
@@ -4,81 +4,92 @@ namespace esphome
 {
     namespace shys_m5_dial
     {
-        class HaDeviceModeClimateTemperature: public esphome::shys_m5_dial::HaDeviceMode {
-            protected:
-                std::string hvac_mode = "none";
-                int current_temperature_ = 0; // Variable to store current temperature
+        class HaDeviceModeClimateTemperature : public esphome::shys_m5_dial::HaDeviceMode
+        {
+        protected:
+            std::string hvac_mode = "none";
+            int current_temperature_ = 0; // Variable to store the current temperature
+            const std::string sensor_entity_id_ = "sensor.dining_room_temperature_2"; // Constant for sensor entity ID
 
-                std::string getHvacMode(){
-                    return this->hvac_mode;
-                }
+            std::string getHvacMode()
+            {
+                return this->hvac_mode;
+            }
 
-                void setHvacMode(const std::string& newMode){
-                    this->hvac_mode = newMode;
-                }
+            void setHvacMode(const std::string &newMode)
+            {
+                this->hvac_mode = newMode;
+            }
 
-                void sendValueToHomeAssistant(int value) override {
-                    haApi.setClimateTemperature(this->device.getEntityId(), value);
-                }
+            void sendValueToHomeAssistant(int value) override
+            {
+                haApi.setClimateTemperature(this->device.getEntityId(), value);
+            }
 
-                void showTemperatureMenu(M5DialDisplay& display){
-                    LovyanGFX* gfx = display.getGfx();
+            void showTemperatureMenu(M5DialDisplay &display)
+            {
+                LovyanGFX *gfx = display.getGfx();
 
-                    uint16_t setTemperature = getValue(); // The set temperature
+                uint16_t setTemperature = getValue(); // The set temperature
 
-                    uint16_t height = gfx->height();
-                    uint16_t width  = gfx->width();
+                uint16_t height = gfx->height();
+                uint16_t width = gfx->width();
 
-                    gfx->setTextColor(MAROON);
-                    gfx->setTextDatum(middle_center);
-                    
-                    gfx->startWrite(); // Secure SPI bus
+                gfx->setTextColor(MAROON);
+                gfx->setTextDatum(middle_center);
 
-                    gfx->fillRect(0, 0, width, this->getDisplayPositionY(setTemperature), RED);
-                    gfx->fillRect(0, this->getDisplayPositionY(setTemperature), width, height, YELLOW);
+                gfx->startWrite(); // Secure SPI bus
 
-                    display.setFontsize(1);
+                gfx->fillRect(0, 0, width, this->getDisplayPositionY(setTemperature), RED);
+                gfx->fillRect(0, this->getDisplayPositionY(setTemperature), width, height, YELLOW);
 
-                    // Display the set temperature
-                    gfx->drawString(("Set: " + String(setTemperature) + "°F").c_str(),
-                                    width / 2,
-                                    height / 2 - 30);
+                display.setFontsize(3);
 
-                    // Display the current temperature from the sensor
-                    gfx->drawString(("Current: " + String(this->current_temperature_) + "°F").c_str(),
-                                    width / 2,
-                                    height / 2 - 60);
+                // Display the set temperature
+                gfx->drawString(("Set: " + String(setTemperature) + "°C").c_str(),
+                                width / 2,
+                                height / 2 - 30);
 
-                    display.setFontsize(1);
+                // Display the current temperature from the sensor
+                gfx->drawString(("Current: " + String(this->current_temperature_) + "°C").c_str(),
+                                width / 2,
+                                height / 2 - 60);
 
-                    gfx->drawString(this->device.getName().c_str(),
-                                    width / 2,
-                                    height / 2 + 20);
-                    gfx->drawString("Temperature",
-                                    width / 2,
-                                    height / 2 + 50);
+                display.setFontsize(1);
 
-                    gfx->endWrite(); // Release SPI bus
-                }
+                gfx->drawString(this->device.getName().c_str(),
+                                width / 2,
+                                height / 2 + 20);
+                gfx->drawString("Temperature",
+                                width / 2,
+                                height / 2 + 50);
 
-            public:
-                HaDeviceModeClimateTemperature(HaDevice& device) : HaDeviceMode(device){
-                    this->maxValue = 80;
-                }
+                gfx->endWrite(); // Release SPI bus
+            }
 
-                void refreshDisplay(M5DialDisplay& display, bool init) override {
-                    this->showTemperatureMenu(display);
-                    ESP_LOGD("DISPLAY", "Temperature-Modus");
-                }
+        public:
+            HaDeviceModeClimateTemperature(HaDevice &device) : HaDeviceMode(device)
+            {
+                this->maxValue = 80;
+            }
 
-                void registerHAListener() override {
-                    std::string attrNameState = "";
-                    api::global_api_server->subscribe_home_assistant_state(
-                                this->device.getEntityId().c_str(),
-                                attrNameState, 
-                                [this](const std::string &state) {
+            void refreshDisplay(M5DialDisplay &display, bool init) override
+            {
+                this->showTemperatureMenu(display);
+                ESP_LOGD("DISPLAY", "Temperature-Modus");
+            }
 
-                        if(this->isValueModified()){
+            void registerHAListener() override
+            {
+                // Subscribe to HVAC mode updates
+                std::string attrNameState = "";
+                api::global_api_server->subscribe_home_assistant_state(
+                    this->device.getEntityId().c_str(),
+                    esphome::optional<std::string>(attrNameState),
+                    [this](const std::string &state)
+                    {
+                        if (this->isValueModified())
+                        {
                             return;
                         }
 
@@ -86,45 +97,54 @@ namespace esphome
                         ESP_LOGI("HA_API", "Got Mode %s for %s", state.c_str(), this->device.getEntityId().c_str());
                     });
 
-                    // Listen for updates from `sensor.dining_room_temperature_2`
-                    std::string sensorEntityId = "sensor.dining_room_temperature_2";
-                    api::global_api_server->subscribe_home_assistant_state(
-                                sensorEntityId.c_str(),
-                                "",
-                                [this](const std::string &state) {
-
+                // Subscribe to the current temperature sensor
+                api::global_api_server->subscribe_home_assistant_state(
+                    this->sensor_entity_id_,
+                    esphome::optional<std::string>(),
+                    [this](const std::string &state)
+                    {
                         auto val = parse_number<float>(state);
 
-                        if (!val.has_value()) {
+                        if (!val.has_value())
+                        {
                             this->current_temperature_ = 0; // Reset if no valid value
-                            ESP_LOGD("HA_API", "No valid temperature value in %s for %s", state.c_str(), sensorEntityId.c_str());
-                        } else {
+                            ESP_LOGD("HA_API", "No valid temperature value in %s for %s", state.c_str(), this->sensor_entity_id_.c_str());
+                        }
+                        else
+                        {
                             this->current_temperature_ = int(val.value());
-                            ESP_LOGI("HA_API", "Got current temperature value %i from %s", int(val.value()), sensorEntityId.c_str());
+                            ESP_LOGI("HA_API", "Got current temperature value %i from %s", int(val.value()), this->sensor_entity_id_.c_str());
                         }
                     });
-                }
+            }
 
-                bool onTouch(M5DialDisplay& display, uint16_t x, uint16_t y) override {
-                    return defaultOnTouch(display, x, y);        
-                }
+            bool onTouch(M5DialDisplay &display, uint16_t x, uint16_t y) override
+            {
+                return defaultOnTouch(display, x, y);
+            }
 
-                bool onRotary(M5DialDisplay& display, const char * direction) override {
-                    return defaultOnRotary(display, direction);
-                }
+            bool onRotary(M5DialDisplay &display, const char *direction) override
+            {
+                return defaultOnRotary(display, direction);
+            }
 
-                bool onButton(M5DialDisplay& display, const char * clickType) override {
-                    if (strcmp(clickType, BUTTON_SHORT) == 0) {
-                        if (strcmp(this->getHvacMode().c_str(), "off") == 0) {
-                            haApi.turnClimateOn(this->device.getEntityId(), "heat");
-                        } else {
-                            haApi.turnClimateOff(this->device.getEntityId());
-                        }
-                        
-                        return true;
-                    } 
-                    return false;
+            bool onButton(M5DialDisplay &display, const char *clickType) override
+            {
+                if (strcmp(clickType, BUTTON_SHORT) == 0)
+                {
+                    if (strcmp(this->getHvacMode().c_str(), "off") == 0)
+                    {
+                        haApi.turnClimateOn(this->device.getEntityId(), "heat");
+                    }
+                    else
+                    {
+                        haApi.turnClimateOff(this->device.getEntityId());
+                    }
+
+                    return true;
                 }
+                return false;
+            }
         };
     }
 }
