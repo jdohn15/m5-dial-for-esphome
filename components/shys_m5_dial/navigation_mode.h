4f4b635eabebbfaa2b46a791c3edf11b26f21193
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-28 23:59:09 -0600

    Update navigation_mode.h

--- components/shys_m5_dial/navigation_mode.h
+++ components/shys_m5_dial/navigation_mode.h
@@ -49,6 +49,7 @@ public:
     }
 
     void update_display_for_selection(M5DialDisplay &display) {
+        ESP_LOGI("NAVIGATION_MODE", "Updating display in navigation mode.");
         // Access the LovyanGFX instance from M5DialDisplay
         LovyanGFX* gfx = display.getGfx();
 
