e286647de144a0bf322abe693926ae33911a45ba
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-28 19:39:45 -0600

    Update ha_device_mode_climate_temperature.h

--- components/shys_m5_dial/ha_device_mode_climate_temperature.h
+++ components/shys_m5_dial/ha_device_mode_climate_temperature.h
@@ -54,7 +54,7 @@ namespace esphome
 
             public:
                 HaDeviceModeClimateTemperature(HaDevice& device) : HaDeviceMode(device){
-                    this->maxValue = 40;
+                    this->maxValue = 80;
                 }
 
                 void refreshDisplay(M5DialDisplay& display, bool init) override {
