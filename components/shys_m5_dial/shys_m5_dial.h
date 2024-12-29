b58468537d6e29ad4a6c45317cdd9b8bd248b5ab
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-29 02:30:51 -0600

    Update shys_m5_dial.h

--- components/shys_m5_dial/shys_m5_dial.h
+++ components/shys_m5_dial/shys_m5_dial.h
@@ -357,25 +357,17 @@ void refreshDisplay(bool forceRefresh) {
 
 
       void turnRotaryRight() {
-          unsigned long now = esphome::millis();
-          if (now - lastRotaryEvent < 10) { // Debounce time of 100ms
-              return;
-          }
-
           m5DialDisplay->resetLastEventTimer();
+          M5Dial.Speaker.tone(5000, 20);
 
           if (navigation_mode_.is_navigation_mode()) {
-              // Exclusively handle navigation mode logic
-              navigation_mode_.handle_rotary_knob(ROTARY_RIGHT, currentDevice, deviceAnzahl);
+              navigation_mode_.handle_rotary_knob(ROTARY_RIGHT, currentDevice);
               refreshDisplay(true); // Force refresh to reflect the change
-          } else if (!navigation_mode_.is_navigation_mode() && m5DialDisplay->isDisplayOn()) {
-              // Exclusively handle regular functionality
-              if (devices[currentDevice] != nullptr) {
-                  devices[currentDevice]->onRotary(*m5DialDisplay, ROTARY_RIGHT);
-              }
+          } else if (m5DialDisplay->isDisplayOn()) {
+              devices[currentDevice]->onRotary(*m5DialDisplay, ROTARY_RIGHT);
           }
 
-          lastRotaryEvent = now;
+          lastRotaryEvent = esphome::millis();
       }
 
       void shortButtonPress() {
