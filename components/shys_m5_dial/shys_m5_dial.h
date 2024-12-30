d0132b720f4c341010b31510049da7f2f03c45ba
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-29 18:06:57 -0600

    Update shys_m5_dial.h

--- components/shys_m5_dial/shys_m5_dial.h
+++ components/shys_m5_dial/shys_m5_dial.h
@@ -347,14 +347,14 @@ namespace esphome {
 
 
       void shortButtonPress() {
-        m5DialDisplay->resetLastEventTimer();
-        M5Dial.Speaker.tone(4000, 20);
-
-        if (navigation_mode_.is_navigation_mode()) {
-          navigation_mode_.exit_navigation_mode();
-        } else if (m5DialDisplay->isDisplayOn()) {
-          devices[currentDevice]->onButton(*m5DialDisplay, BUTTON_SHORT);
-        }
+          m5DialDisplay->resetLastEventTimer();
+          M5Dial.Speaker.tone(4000, 20);
+      
+          if (navigation_mode_.is_navigation_mode()) {
+              navigation_mode_.exit_navigation_mode(*m5DialDisplay, currentDevice, devices);
+          } else if (m5DialDisplay->isDisplayOn()) {
+              devices[currentDevice]->onButton(*m5DialDisplay, BUTTON_SHORT);
+          }
       }
 
       void longButtonPress() {
