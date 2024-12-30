4276d2a11c6b6f6f7f3b97c8d6437c214413540b
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-29 20:19:38 -0600

    Update ha_device_mode_switch_on_off.h

--- components/shys_m5_dial/ha_device_mode_switch_on_off.h
+++ components/shys_m5_dial/ha_device_mode_switch_on_off.h
@@ -22,12 +22,12 @@ namespace esphome
                     uint16_t height = gfx->height();
                     uint16_t width  = gfx->width();
 
-                    gfx->setTextColor(MAROON);
+                    gfx->setTextColor(WHITE);
                     gfx->setTextDatum(middle_center);
 
                     gfx->startWrite();                      // Secure SPI bus
 
-                    gfx->fillRect(0, 0, width, height, currentValue>0?YELLOW:RED);
+                    gfx->fillRect(0, 0, width, height, currentValue>0?RED:BLACK);
 
                     display.setFontsize(3);
                     gfx->drawString(currentValue>0?"on":"off",
