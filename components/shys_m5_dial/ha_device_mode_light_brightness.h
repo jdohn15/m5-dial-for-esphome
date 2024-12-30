034d1503fd99b31323805b8d87455c0bdcfe25f0
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2024-12-29 20:09:58 -0600

    Update ha_device_mode_light_brightness.h

--- components/shys_m5_dial/ha_device_mode_light_brightness.h
+++ components/shys_m5_dial/ha_device_mode_light_brightness.h
@@ -16,13 +16,13 @@ namespace esphome
                     uint16_t height = gfx->height();
                     uint16_t width  = gfx->width();
 
-                    gfx->setTextColor(MAROON);
+                    gfx->setTextColor(WHITE);
                     gfx->setTextDatum(middle_center);
 
                     gfx->startWrite();                      // Secure SPI bus
 
-                    gfx->fillRect(0, 0, width, this->getDisplayPositionY(currentValue), RED);
-                    gfx->fillRect(0, this->getDisplayPositionY(currentValue), width, height, YELLOW);
+                    gfx->fillRect(0, 0, width, this->getDisplayPositionY(currentValue), BLACK);
+                    gfx->fillRect(0, this->getDisplayPositionY(currentValue), width, height, RED);
 
                     display.setFontsize(3);
                     gfx->drawString((String(currentValue) + "%").c_str(),
